fe516af62aa74c29dbccef84d47403e16f0f2823
yubawarabi <67773546+yubawarabi@users.noreply.github.com>
2020-07-11 13:53:23 +0900

    Update 0710.py
    apple_priceについていた""を外しました
    円を追加しました
    代金が整数で表示されるようにしました

--- 0710.py
+++ 0710.py
@@ -1,4 +1,4 @@
 #8%税込代金を表示したい
 apple_price = 50
 apple_number = 12
-print("ひとつ", "apple_price", "のりんごが", apple_number, "個で", apple_price*apple_number*1.08, "円です。") 
+print("ひとつ", apple_price, "円のりんごが", apple_number, "個で", int(apple_price*apple_number*1.08), "円です。") 
